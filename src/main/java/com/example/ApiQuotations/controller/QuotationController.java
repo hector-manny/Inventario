a0b7aa3a80ab403383cd3d0d44c7501de1e7516f
snaven10 <frjsamuel@outlook.com>
2024-11-20 14:30:23 -0600

    mergin

--- src/main/java/com/example/ApiQuotations/controller/QuotationController.java
+++ src/main/java/com/example/ApiQuotations/controller/QuotationController.java
@@ -1,8 +1,8 @@
 package com.example.ApiQuotations.controller;
 
 import java.util.List;
+import java.util.Map;
 
-import org.hibernate.query.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,24 +13,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ApiQuotations.model.Quotation;
 import com.example.ApiQuotations.service.QuotationService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/quotes")
+@RequestMapping("/api/v1/quotations")
 @RequiredArgsConstructor
 public class QuotationController {
-    private QuotationService quotationService;
-    
 
-    @GetMapping
-    public ResponseEntity<List<com.example.ApiQuotations.model.Quotation>> getAllQuotations() {
-        return ResponseEntity.ok(quotationService.getAllQuotations());
+    private final QuotationService quotationService;
+
+   
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> createQuotation(
+            @RequestParam Long productId,
+            @RequestBody Quotation quotation) {
+        Map<String, Object> response = quotationService.saveQuotation(quotation, productId);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    @PostMapping
-    public ResponseEntity<com.example.ApiQuotations.model.Quotation> createQuotation(@RequestBody com.example.ApiQuotations.model.Quotation request) {
-        return new ResponseEntity<>(quotationService.saveQuotation(request), HttpStatus.CREATED);
+    @GetMapping
+    public ResponseEntity<List<Quotation>> getAllQuotations() {
+        List<Quotation> quotations = quotationService.getAllQuotations();
+        return ResponseEntity.ok(quotations);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> getQuotationById(@PathVariable Integer id) {
+        Map<String, Object> response = quotationService.getQuotationDetailsById(id);
+        return ResponseEntity.ok(response);
     }
 }
