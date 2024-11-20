d431be3e9efc4e30a6115bb45dd61a7653275a31
hector martinez <mejia.martinez2012@hotmail.es>
2024-11-20 09:34:06 -0600

    enpoint para registro y obtener productos

---
+++ src/main/java/com/example/ApiQuotations/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.ApiQuotations.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ApiQuotations.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+    
+} 
+
---
+++ src/main/java/com/example/ApiQuotations/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.example.ApiQuotations.service;
+
+import com.example.ApiQuotations.model.Product;
+import com.example.ApiQuotations.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
